f3a15fb86757c94751cf44107c22b300585745d9
stickland-ehp <32290877+stickland-ehp@users.noreply.github.com>
2024-02-14 16:18:54 +1000

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -3,67 +3,60 @@ from bs4.element import Comment
 import pandas as pd
 import numpy as np
 import requests
+import json
 
 final_data = pd.DataFrame()
+
 for i in range(10):
-    
     url = "https://parks.des.qld.gov.au/xml/parkalerts.xml".format(i+1)
-    
     xml_data = requests.get(url).content
 
     soup = BeautifulSoup(xml_data, "xml")
-     
+
     # Find all text in the data
     texts = str(soup.findAll(text=True)).replace('\\n','')
-    
-    #Find the tag/child
+
+    # Find the tag/child
     child = soup.find("alert")
-    
+
     title = []
-    url = []
+    alert_url = []
     description = []
-    details = []
-    date_start = []
-    date_end = []
-    date_effective = []
-    parks = []
-    
-    while True:               
+    details= []
+
+     while True:
         try:
-            title.append(" ".join(child.find('title')))
+            title.append(" ".join(child.find('title').text))
         except:
             title.append(" ")
-        
-        try:
-            url.append(" ".join(child.find('url')))
-        except:
-            url.append(" ")
-            
-        try:
-            description.append(" ".join(child.find('descrption')))
-        except:
-            description.append(" ")
-        
-        try:
-            details.append(" ".join(child.find('details')))
-        except:
-            details.append(" ")
-        
-        try:   
-            # Next sibling of child, here: entry 
-            child = child.find_next_sibling('alert')
-        except:
-            break
-    
-    data = []
-    data = pd.DataFrame({
-      "title": title,
-      "url": url,
-      "description": description,
-      "details": details
-    })
-    final_data = final_data.append(data, ignore_index = True)
 
-    # Print the final_data to file
-    with open("alerts.json", "w") as f:
-        print(json.dumps(final_data), file=f)
+        try:
+            alert_url.append(" ".join(child.find('url').text))
+        except:
+            alert_url.append(" ")
+
+        try:
+            description.append(" ".join(child.find('description').text))
+       except:
+           description.append(" ")
+
+       try:   
+          details.append(" ".join(child.find('details').text))
+      except:
+          details.append(" ")
+    
+      # Next sibling of child, here: entry 
+      child = child.find_next_sibling('alert')
+      if not child:  # Use break statement if no more alerts found.
+          break
+    
+   data=pd.DataFrame({
+         "title":title,
+         "url":alert_url,
+         "description":description,
+         "details":details})
+
+   final_data=final_data.append(data, ignore_index=True)
+
+# Print the final_data to file (JSON format)
+final_data.to_json(r'alerts.json', orient='records')
